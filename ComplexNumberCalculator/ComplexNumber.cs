acae8e5f9fdb2bcbcbd0727ede479e39bfaf6e9d
Maksym Ivanov <sinnwidriger@gmail.com>
2022-10-09 00:28:17 +0200

    Add ComplexNumber class with basic operators and ToString method

---
+++ ComplexNumberCalculator/ComplexNumber.cs
@@ -0,0 +1,48 @@
+﻿namespace ComplexNumberCalculator
+{
+    public class ComplexNumber
+    {
+        public ComplexNumber(double realPart, double imaginaryPart)
+        {
+            this.RealPart = realPart;
+            this.ImaginaryPart = imaginaryPart;
+        }
+
+        public double RealPart { get; set; }
+        public double ImaginaryPart { get; set; }
+
+        public ComplexNumber Add(ComplexNumber rhs)
+        {
+            return new ComplexNumber(this.RealPart + rhs.RealPart, this.ImaginaryPart + rhs.ImaginaryPart);
+        }
+
+        public ComplexNumber Minus(ComplexNumber rhs)
+        {
+            return new ComplexNumber(this.RealPart - rhs.RealPart, this.ImaginaryPart - rhs.ImaginaryPart);
+        }
+
+        public ComplexNumber Multiply(ComplexNumber rhs)
+        {
+            return new ComplexNumber(
+                this.RealPart * rhs.RealPart - this.ImaginaryPart * rhs.ImaginaryPart,  // Real Part = ac - bd
+                this.RealPart * rhs.ImaginaryPart + this.ImaginaryPart * rhs.RealPart); // Imaginary Part = ad + bc
+        }
+
+        public ComplexNumber Divide(ComplexNumber rhs)
+        {
+            double realPart =
+                (this.RealPart * rhs.RealPart - this.ImaginaryPart * rhs.ImaginaryPart) /
+                (rhs.RealPart * rhs.RealPart + rhs.ImaginaryPart * rhs.ImaginaryPart); // Real Part = (ac + bd) / (c^2 + d^2)
+            double imaginaryPart =
+                (this.ImaginaryPart * rhs.RealPart - this.RealPart * rhs.ImaginaryPart) /
+                (rhs.RealPart * rhs.RealPart + rhs.ImaginaryPart * rhs.ImaginaryPart); // Imaginary Part = (bc - ad) / (c^2 + d^2)
+
+            return new ComplexNumber(realPart, imaginaryPart);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.RealPart} + {this.ImaginaryPart}i";
+        }
+    }
+}
--- ComplexNumberCalculator/Program.cs
+++ ComplexNumberCalculator/Program.cs
@@ -4,6 +4,9 @@
     {
         private static void Main(string[] args)
         {
+            //ComplexNumber n1 = new ComplexNumber(3, 4);
+            //ComplexNumber n2 = new ComplexNumber(5, 6);
+            //Console.WriteLine(n1.Multiply(n2));
         }
     }
 }
