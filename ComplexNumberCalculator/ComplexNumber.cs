1dd9049af0e35ad4ac41ca2c3e77c638152d9ff6
Maksym Ivanov <sinnwidriger@gmail.com>
2022-10-09 22:42:39 +0200

    Add radius and theta properties with recalculation, add pow and sqrt functions

--- ComplexNumberCalculator/ComplexNumber.cs
+++ ComplexNumberCalculator/ComplexNumber.cs
@@ -1,4 +1,6 @@
-﻿namespace ComplexNumberCalculator
+﻿using System.Runtime.InteropServices;
+
+namespace ComplexNumberCalculator
 {
     public class ComplexNumber
     {
@@ -6,8 +8,18 @@
 
         public ComplexNumber(double realPart, double imaginaryPart)
         {
-            this.RealPart = realPart;
-            this.ImaginaryPart = imaginaryPart;
+            _realPart = realPart;
+            _imaginaryPart = imaginaryPart;
+            recalculateRadius();
+            recalculateTheta();
+        }
+
+        public ComplexNumber(ComplexNumber copy)
+        {
+            this._realPart = copy._realPart;
+            this._imaginaryPart = copy._imaginaryPart;
+            this._radius = copy._radius;
+            this._theta = copy._theta;
         }
 
         public ComplexNumber(string input)
@@ -15,8 +27,10 @@
             try
             {
                 Tuple<double, double> numberPair = parseComplexNumber(input);
-                this.RealPart = numberPair.Item1;
-                this.ImaginaryPart = numberPair.Item2;
+                _realPart = numberPair.Item1;
+                _imaginaryPart = numberPair.Item2;
+                recalculateRadius();
+                recalculateTheta();
             }
             catch
             {
@@ -29,9 +43,69 @@
 
         #region Properties
 
-        public double RealPart { get; set; }
-        public double ImaginaryPart { get; set; }
         public static int Precision { get; set; } = 2;
+
+        private double _realPart;
+
+        public double RealPart
+        {
+            get
+            {
+                return _realPart;
+            }
+            set
+            {
+                _realPart = value;
+                recalculateRadius();
+                recalculateTheta();
+            }
+        }
+
+        private double _imaginaryPart;
+
+        public double ImaginaryPart
+        {
+            get
+            {
+                return _imaginaryPart;
+            }
+            set
+            {
+                _imaginaryPart = value;
+                recalculateRadius();
+                recalculateTheta();
+            }
+        }
+
+        private double _radius;
+
+        public double Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = value;
+                recalculateRealAndImaginaryPart();
+            }
+        }
+
+        private double _theta;
+
+        public double Theta
+        {
+            get
+            {
+                return _theta;
+            }
+            set
+            {
+                _theta = value;
+                recalculateRealAndImaginaryPart();
+            }
+        }
 
         #endregion Properties
 
@@ -58,12 +132,28 @@
         {
             double realPart =
                 (this.RealPart * rhs.RealPart - this.ImaginaryPart * rhs.ImaginaryPart) /
-                (rhs.RealPart * rhs.RealPart + rhs.ImaginaryPart * rhs.ImaginaryPart); // Real Part = (ac + bd) / (c^2 + d^2)
+                (rhs.RealPart * rhs.RealPart + rhs.ImaginaryPart * rhs.ImaginaryPart);     // Real Part = (ac + bd) / (c^2 + d^2)
             double imaginaryPart =
                 (this.ImaginaryPart * rhs.RealPart - this.RealPart * rhs.ImaginaryPart) /
-                (rhs.RealPart * rhs.RealPart + rhs.ImaginaryPart * rhs.ImaginaryPart); // Imaginary Part = (bc - ad) / (c^2 + d^2)
+                (rhs.RealPart * rhs.RealPart + rhs.ImaginaryPart * rhs.ImaginaryPart);     // Imaginary Part = (bc - ad) / (c^2 + d^2)
 
             return new ComplexNumber(realPart, imaginaryPart);
+        }
+
+        public ComplexNumber Pow(int n)
+        {
+            ComplexNumber cn = new ComplexNumber(this);
+            cn.Radius = Math.Pow(cn.Radius, n);
+            cn.Theta *= n;
+            return cn;
+        }
+
+        public ComplexNumber Sqrt(int n)
+        {
+            ComplexNumber cn = new ComplexNumber(this);
+            cn.Radius = Math.Pow(cn.Radius, 1.0 / n);
+            cn.Theta /= n;
+            return cn;
         }
 
         #endregion Extension Methods
@@ -90,6 +180,16 @@
             return lhs.Divide(rhs);
         }
 
+        public static ComplexNumber Pow(ComplexNumber lhs, int n)
+        {
+            return lhs.Pow(n);
+        }
+
+        public static ComplexNumber Sqrt(ComplexNumber lhs, int n)
+        {
+            return lhs.Sqrt(n);
+        }
+
         #endregion Static Methods
 
         #region Overloaded Operators
@@ -112,6 +212,14 @@
         public static ComplexNumber operator /(ComplexNumber lhs, ComplexNumber rhs)
         {
             return lhs.Divide(rhs);
+        }
+
+        public static ComplexNumber operator ^(ComplexNumber lhs, double n)
+        {
+            if (n > 1)
+                return lhs.Pow((int)n);
+            else
+                return lhs.Sqrt((int)(1 / n));
         }
 
         #endregion Overloaded Operators
@@ -143,6 +251,26 @@
             string[] splited = input.Split(separators, 2, StringSplitOptions.RemoveEmptyEntries);
 
             return new Tuple<double, double>(Double.Parse(splited[0]), sign * Double.Parse(splited[1]));
+        }
+
+        private void recalculateRadius()
+        {
+            _radius = Math.Sqrt(RealPart * RealPart + ImaginaryPart * ImaginaryPart);
+        }
+
+        private void recalculateTheta()
+        {
+            _theta = Math.Atan(ImaginaryPart / RealPart);
+        }
+
+        private void recalculateRealAndImaginaryPart()
+        {
+            _realPart = Math.Sqrt(                  // new Radius and old Theta
+                (Radius * Radius) /                 // a = ( r^2 / (1 + tg(Theta)^2) )^1/2
+                (1 + Math.Pow(Math.Tan(Theta), 2))  //
+            );
+
+            _imaginaryPart = RealPart * Math.Tan(Theta); // b = a * tg(Theta)
         }
 
         #endregion Private Methods
