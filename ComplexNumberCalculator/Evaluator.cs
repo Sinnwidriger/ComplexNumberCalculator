70e8847639eb38cfeb1ca0760917fc1694cb0356
Maksym Ivanov <sinnwidriger@gmail.com>
2022-10-10 20:30:22 +0200

    Add expression evaluator class

---
+++ ComplexNumberCalculator/Evaluator.cs
@@ -0,0 +1,44 @@
+﻿namespace ComplexNumberCalculator
+{
+    public class Evaluator
+    {
+        public static ComplexNumber Evaluate(string expression)
+        {
+            string[] operators = new string[] { "+", "-", "*", "/", "^" };
+            char[] separators = new char[] { '(', ')' };
+            string[] expressionParts = expression.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            ComplexNumber result = new ComplexNumber(expressionParts[0]);
+            for (int i = 1; i < expressionParts.Length; i++)
+            {
+                if (operators.Contains(expressionParts[i]))
+                {
+                    switch (expressionParts[i])
+                    {
+                        case "+":
+                            result += new ComplexNumber(expressionParts[i + 1]);
+                            break;
+
+                        case "-":
+                            result -= new ComplexNumber(expressionParts[i + 1]);
+                            break;
+
+                        case "*":
+                            result *= new ComplexNumber(expressionParts[i + 1]);
+                            break;
+
+                        case "/":
+                            result /= new ComplexNumber(expressionParts[i + 1]);
+                            break;
+
+                        case "^":
+                            result ^= Double.Parse(expressionParts[i + 1]);
+                            break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- ComplexNumberCalculator/Program.cs
+++ ComplexNumberCalculator/Program.cs
@@ -4,55 +4,56 @@
     {
         private static void Main(string[] args)
         {
-            //if (args.Length == 0)
-            //{
-            //    Console.WriteLine("Too few arguments");
-            //    Console.WriteLine("Type --help for help");
-            //    Environment.Exit(0);
-            //}
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Too few arguments");
+                Console.WriteLine("Type --help for help");
+                Environment.Exit(0);
+            }
 
-            //if (args[0] == "--help")
-            //{
-            //    Console.WriteLine("Here is your help");
-            //    Environment.Exit(0);
-            //}
+            if (args[0] == "--help")
+            {
+                Console.WriteLine("Here is your help");
+                Environment.Exit(0);
+            }
 
-            //if (args.Length > 1)
-            //{
-            //    if (args[1] == "-p")
-            //    {
-            //        if (args.Length > 2)
-            //        {
-            //            try
-            //            {
-            //                int precision = Convert.ToInt32(args[2]);
-            //                ComplexNumber.Precision = precision;
-            //            }
-            //            catch
-            //            {
-            //                Console.WriteLine("Invalid precision provided");
-            //                Environment.Exit(0);
-            //            }
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("No value for precision provided");
-            //            Environment.Exit(0);
-            //        }
-            //    }
+            if (args.Length > 1)
+            {
+                if (args[1] == "-p")
+                {
+                    if (args.Length > 2)
+                    {
+                        try
+                        {
+                            int precision = Convert.ToInt32(args[2]);
+                            ComplexNumber.Precision = precision;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Invalid precision provided");
+                            Environment.Exit(0);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No value for precision provided");
+                        Environment.Exit(0);
+                    }
+                }
 
-            //    if (args.Length > 3)
-            //    {
-            //        Console.WriteLine("Invalid arguments provided");
-            //        foreach (string argument in args.Skip(3))
-            //        {
-            //            Console.WriteLine($"Argument \"{argument}\" is not recognized or is not necessary");
-            //        }
-            //        Environment.Exit(0);
-            //    }
-            //}
-            ComplexNumber cn = new ComplexNumber(3, 4);
-            Console.WriteLine(cn.Sqrt(2));
+                if (args.Length > 3)
+                {
+                    Console.WriteLine("Invalid arguments provided");
+                    foreach (string argument in args.Skip(3))
+                    {
+                        Console.WriteLine($"Argument \"{argument}\" is not recognized or is not necessary");
+                    }
+                    Environment.Exit(0);
+                }
+            }
+
+            ComplexNumber evaluationResult = Evaluator.Evaluate(args[0]);
+            Console.WriteLine(evaluationResult);
         }
     }
 }
